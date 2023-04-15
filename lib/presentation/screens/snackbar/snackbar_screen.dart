e847ca252d3cd853b27bd482b2b9f46ed28d4284
martynvalenz <martynvalenz@gmail.com>
2023-04-15 14:52:50 -0600

    screens created and barrel

---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SnacbarScreen extends StatelessWidget {
+  static const String name = 'snacbar_screen';
+  const SnacbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
