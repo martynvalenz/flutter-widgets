9d867d5d1c064ab9eb9920536eb2e05779fe798d
martynvalenz <martynvalenz@gmail.com>
2023-04-18 19:43:16 -0600

    ...

--- lib/presentation/screens/theme_changer/theme_changer_screen.dart
+++ lib/presentation/screens/theme_changer/theme_changer_screen.dart
@@ -39,7 +39,7 @@ class _ThemeChangerView extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final List<Color> colors = ref.watch(colorListProvider);
     // final int selectedColor = ref.watch(selectedColorProvider);
-    final selectedColor = ref.watch(themeNotifierProvider).selectedColor;
+    final int selectedColor = ref.watch(themeNotifierProvider).selectedColor;
 
     return ListView.builder(
       itemCount: colors.length,
